b9f42bd873315d61b2af6c87943ec117efe64b79
frankyyyyy <fanlijun3@126.com>
2018-07-07 19:17:37 +1000

    colorlist component created
    much more organized

--- index.js
+++ index.js
@@ -1,83 +1,18 @@
 import React, { Component } from 'react';
 
 import {
-    StyleSheet,
     AppRegistry,
-    ListView
+    Alert
 } from 'react-native'
 
-import ColorButton from './components/ColorButton'
-import ColorForm from './components/ColorForm'
+import ColorList from './components/ColorList'
 
-class App extends React.Component{
-
-    constructor(){
-        super();
-
-        this.ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        })
-        const availableColors = [
-            'red',
-            'green',
-            'yellow',
-            'salmon',
-            'black',
-            'white',
-            'pink']
-        this.state = {
-            backgroundColor: 'blue',
-            availableColors,
-            dataSource: this.ds.cloneWithRows(availableColors)
-        }
-        this.changeColor = this.changeColor.bind(this)
-        this.newColor = this.newColor.bind(this)
-    }
-
-    changeColor(backgroundColor){
-        this.setState({backgroundColor})
-    }
-
-    newColor(color){
-        const availableColors = [
-            ...this.state.availableColors,
-            color
-        ]
-        this.setState({
-            availableColors,
-            dataSource: this.ds.cloneWithRows(availableColors)
-        })
-    }
-
+class App extends Component{
     render(){
-        const { backgroundColor, dataSource } = this.state
         return (
-            <ListView style={[ styles.container, {backgroundColor: backgroundColor} ]}
-                    dataSource={dataSource}
-                    renderRow={(color) => (
-                        <ColorButton backgroundColor = {color}
-                                     onSelect={this.changeColor}/>
-                    )}
-                    renderHeader={() => (
-                       <ColorForm onNewColor={this.newColor}/>
-                    )}>
-
-            </ListView>
+            <ColorList onColorSelected={ color => Alert.alert(color) }/>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    },
-    header:{
-        backgroundColor: 'lightgrey',
-        paddingTop: 20,
-        padding: 10,
-        fontSize: 30,
-        textAlign: 'center'
-    }
-})
 
 AppRegistry.registerComponent('color_demo', () => App);
