5c3463c47ae30c3741bda7ada48c22c758eeaeb3
frankyyyyy <fanlijun3@126.com>
2018-07-16 18:04:11 +1000

    Revert "colorlist component created"
    This reverts commit b9f42bd873315d61b2af6c87943ec117efe64b79.

--- index.js
+++ index.js
@@ -1,18 +1,83 @@
 import React, { Component } from 'react';
 
 import {
+    StyleSheet,
     AppRegistry,
-    Alert
+    ListView
 } from 'react-native'
 
-import ColorList from './components/ColorList'
+import ColorButton from './components/ColorButton'
+import ColorForm from './components/ColorForm'
 
-class App extends Component{
+class App extends React.Component{
+
+    constructor(){
+        super();
+
+        this.ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+        const availableColors = [
+            'red',
+            'green',
+            'yellow',
+            'salmon',
+            'black',
+            'white',
+            'pink']
+        this.state = {
+            backgroundColor: 'blue',
+            availableColors,
+            dataSource: this.ds.cloneWithRows(availableColors)
+        }
+        this.changeColor = this.changeColor.bind(this)
+        this.newColor = this.newColor.bind(this)
+    }
+
+    changeColor(backgroundColor){
+        this.setState({backgroundColor})
+    }
+
+    newColor(color){
+        const availableColors = [
+            ...this.state.availableColors,
+            color
+        ]
+        this.setState({
+            availableColors,
+            dataSource: this.ds.cloneWithRows(availableColors)
+        })
+    }
+
     render(){
+        const { backgroundColor, dataSource } = this.state
         return (
-            <ColorList onColorSelected={ color => Alert.alert(color) }/>
+            <ListView style={[ styles.container, {backgroundColor: backgroundColor} ]}
+                    dataSource={dataSource}
+                    renderRow={(color) => (
+                        <ColorButton backgroundColor = {color}
+                                     onSelect={this.changeColor}/>
+                    )}
+                    renderHeader={() => (
+                       <ColorForm onNewColor={this.newColor}/>
+                    )}>
+
+            </ListView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+    header:{
+        backgroundColor: 'lightgrey',
+        paddingTop: 20,
+        padding: 10,
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
 
 AppRegistry.registerComponent('color_demo', () => App);
